fe8e2876e73d81f16d3418e1a4c772d58e7b1495
Rajhesh R <41332510+RajheshRavi@users.noreply.github.com>
2025-01-12 01:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+ccc# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -38,6 +38,8 @@ if ingredient_list :
     ingredients = ''
     for fruit in ingredient_list:
         ingredients += fruit + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit,' is ', search_on, '.')
         st.subheader(fruit+' Nutrition Information')
         my_smoothiee_fruit = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit)
         sf_df = st.dataframe(data=my_smoothiee_fruit.json(), use_container_width = True)
